bfe56b6f7d862e4079a984c7012f1361016a8bd3
Guy <dotat@hotmail.com>
2023-01-15 14:55:39 -0800

    Add color primitive

---
+++ pyray/colors.py
@@ -0,0 +1,8 @@
+class Color:
+    """A composite of three colors: red, green, and blue. Each be a value
+    between 0 and 1"""
+
+    def __init__(self, red: float, green: float, blue: float):
+        self.red = red
+        self.green = green
+        self.blue = blue
